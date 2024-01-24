2a96be513da5cbd9ae3a800139728027366b7f17
daniel <daniels302@gmail.com>
2024-01-24 08:39:38 +0200

    first version of puzzle 3

--- advent of code 2023/puzzle 3/puzzle 3/Program.cs
+++ advent of code 2023/puzzle 3/puzzle 3/Program.cs
@@ -7,20 +7,39 @@ namespace puzzle_3
         static void Main(string[] args)
         {
             string path = "C:\\Programming Projects\\advent of code 2023\\puzzle 3\\puzzle 3\\input.txt";
-            var inputCollection = File.ReadLines(path).ToList();
+            char[,] charMatrix = new char[File.ReadAllLines(path).Length, File.ReadAllLines(path)[0].Length];
+            int numOfLines = File.ReadAllLines(path).Length;
+            int numOfCharsInLine = File.ReadAllLines(path)[0].Length;
+            using (var reader = new StreamReader(path))
+            {
+                int boo, i = 0, j = 0;
+                
+                while ((boo = reader.Read()) !=-1) 
+                {
+                    Console.WriteLine($"this is the {i} {j} iteration");
+                    if (j == numOfCharsInLine)
+                    {
+                        j = 0; i++;
+                    }
+                    if (i == numOfLines)
+                    { break; }
+                    
+                    charMatrix[i,j] = (char)boo;  
+                    j++;
+                }
+            }
 
-            var regex = new Regex("(\\d+)|[$@/*]");
-            var collection = inputCollection.Where(s => regex.IsMatch(s)).Select(strng => regex.Matches(strng)).
-                Select(s => s.Select(match => new { 
-                    index = match.Index,
-                    value = match.Value,
-                    isNumber = int.TryParse(match.Value,out int a)
-                })
-                ).ToList();
+            Console.WriteLine("sd");
+
+
+
 
 
 
 
         }
+
+           
+        
     }
 }
